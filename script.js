fc69008f11c842874f27a802f6273a711607084c
Jocelyn Lahaise <wlahaise@albany.edu>
2024-02-28 23:55:52 -0500

    starting to set buttons

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+    document.getElementById("downloadButton").addEventListener("click", print);
+    document.getElementById("previewButton").addEventListener("click", print);
+}
+
 function print() {
     console.log("hello wordlddd");
 }
