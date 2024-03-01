e2e68aab042c7f1c96c76fe00f9d7af970006354
Jocelyn409 <jocelynlahaise@gmail.com>
2024-03-01 02:07:10 -0500

    got radio buttons working; trying to get selection highlight for them working.

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-window.onload = function() {
-    document.getElementById("downloadButton").addEventListener("click", print);
-    document.getElementById("previewButton").addEventListener("click", print);
-    document.getElementById("previewButton").addEventListener("click", setRadioButton);
-}
+var enhanceX2 = document.getElementById("enhanceX2");
+var enhanceX3 = document.getElementById("enhanceX3");
+var enhanceX4 = document.getElementById("enhanceX4");
+
+var radioTwo = document.getElementById("radioTwo");
+var radioThree = document.getElementById("radioThree");
+var radioFour = document.getElementById("radioFour");
+
+var prevSelectedZoom = radioThree; // Pretend the value is from cookies/last enhancement level user selected
+setRadioButton(prevSelectedZoom);
+
+document.getElementById("downloadButton").addEventListener("click", print);
+document.getElementById("previewButton").addEventListener("click", print);
+
+enhanceX2.addEventListener("click", function() { setRadioButton(radioTwo); });
+enhanceX3.addEventListener("click", function() { setRadioButton(radioThree); });
+enhanceX4.addEventListener("click", function() { setRadioButton(radioFour); });
+
+document.querySelectorAll('input[name="enhanceLevel"]').forEach((elem) => {
+    elem.addEventListener("change", function(event) {
+        console.log(event);
+    });
+});
 
 function print() {
-    console.log("hello wordlddd");
+    console.log("hello worlddd");
 }
 
-function setRadioButton() {
-    // Find the radio button with the specified ID
-    var radioButton = document.getElementById("two");
+function setRadioButton(radioInput) {
+    radioInput.checked = true;
+}
 
-    // Check the radio button
-    radioButton.checked = true;
-    console.log(radioButton.checked);
+function changeSelectedColor(radioInput, buttonInput) {
+    console.log("hai");
+    if(radioInput.checked) {
+        buttonInput.style.color = "red";
+    }
+    else {
+        buttonInput.style.color = "blue";
+
+    }
 }
