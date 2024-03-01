a3a5b513159fdc8a0f3057086d7a49f3996d85ef
Jocelyn409 <jocelynlahaise@gmail.com>
2024-03-01 12:03:13 -0500

    still workin on it

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-var enhanceX2 = document.getElementById("enhanceX2");
-var enhanceX3 = document.getElementById("enhanceX3");
-var enhanceX4 = document.getElementById("enhanceX4");
-
-var radioTwo = document.getElementById("radioTwo");
-var radioThree = document.getElementById("radioThree");
-var radioFour = document.getElementById("radioFour");
-
-var prevSelectedZoom = radioThree; // Pretend the value is from cookies/last enhancement level user selected
-setRadioButton(prevSelectedZoom);
-
-document.getElementById("downloadButton").addEventListener("click", print);
-document.getElementById("previewButton").addEventListener("click", print);
-
-enhanceX2.addEventListener("click", function() { setRadioButton(radioTwo); });
-enhanceX3.addEventListener("click", function() { setRadioButton(radioThree); });
-enhanceX4.addEventListener("click", function() { setRadioButton(radioFour); });
-
-document.querySelectorAll('input[name="enhanceLevel"]').forEach((elem) => {
-    elem.addEventListener("change", function(event) {
-        console.log(event);
+window.onload = function() {
+    var enhanceX2 = document.getElementById("enhanceX2");
+    var enhanceX3 = document.getElementById("enhanceX3");
+    var enhanceX4 = document.getElementById("enhanceX4");
+    
+    var radioTwo = document.getElementById("radioTwo");
+    var radioThree = document.getElementById("radioThree");
+    var radioFour = document.getElementById("radioFour");
+    
+    var prevSelectedZoom = radioThree; // Pretend the value is from cookies/last enhancement level user selected
+    setRadioButton(prevSelectedZoom);
+    
+    document.getElementById("downloadButton").addEventListener("click", print);
+    document.getElementById("previewButton").addEventListener("click", print);
+    
+    enhanceX2.addEventListener("click", function() { setRadioButton(radioTwo); });
+    enhanceX3.addEventListener("click", function() { setRadioButton(radioThree); });
+    enhanceX4.addEventListener("click", function() { setRadioButton(radioFour); });
+    
+    document.querySelectorAll('input[name="enhanceLevel"]').forEach((elem) => {
+        elem.addEventListener("change", function(event) {
+            console.log(event);
+        });
     });
-});
+}
 
 function print() {
     console.log("hello worlddd");
@@ -28,15 +30,15 @@ function print() {
 
 function setRadioButton(radioInput) {
     radioInput.checked = true;
+    if(radioFour.checked); {
+        enhanceX2.style.color = "red";
+    }
 }
 
-function changeSelectedColor(radioInput, buttonInput) {
+function changeSelectedColor(buttonInput) {
     console.log("hai");
-    if(radioInput.checked) {
-        buttonInput.style.color = "red";
-    }
-    else {
-        buttonInput.style.color = "blue";
-
-    }
+    buttonInput.style.color = "red";
 }
+
+// send currently selected enhance level to storage
+console.log("hai!!! :3");
