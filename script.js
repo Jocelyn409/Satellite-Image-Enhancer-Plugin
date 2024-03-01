8ca30140be63fea2550cdd0a7187aa4fe5f8d621
Jocelyn Lahaise <wlahaise@albany.edu>
2024-03-01 18:03:36 -0500

    finished keeping enhance level

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ window.onload = function() {
     var radioTwo = document.getElementById("radioTwo");
     var radioThree = document.getElementById("radioThree");
     var radioFour = document.getElementById("radioFour");
-    
-    var prevSelectedZoom = radioThree; // Pretend the value is from cookies/last enhancement level user selected
-    setRadioButton(prevSelectedZoom, 3);
+
+    chrome.storage.sync.get(["radioSetting", "buttonSetting"]).then((result) => {
+        console.log("Value is " + result.radioSetting + " and " + result.buttonSetting);
+        if(!result.radioSetting || !result.buttonSetting) {
+            setRadioButton(radioThree, 3);
+        }
+        else {
+            setRadioButton(result.radioSetting, result.buttonSetting);
+        }
+    });
     
     document.getElementById("downloadButton").addEventListener("click", print);
     document.getElementById("previewButton").addEventListener("click", print);
@@ -32,12 +39,22 @@ function setRadioButton(radioInput, buttonInput) {
     
     switch(buttonInput) {
         case 2: enhanceX2.style.border = "5px inset orange";
+        chrome.storage.sync.set({ radioSetting: "radioTwo", buttonSetting: 2 }).then(() => {
+            console.log("Value is set");
+        });
         break;
         case 3: enhanceX3.style.border = "5px inset orange";
+        chrome.storage.sync.set({ radioSetting: "radioThree", buttonSetting: 3 }).then(() => {
+            console.log("Value is set");
+        });
         break;
         case 4: enhanceX4.style.border = "5px inset orange";
+        chrome.storage.sync.set({ radioSetting: "radioFour", buttonSetting: 4 }).then(() => {
+            console.log("Value is set");
+        });
         break;
     }
+    
 }
 
 // send currently selected enhance level to storage
