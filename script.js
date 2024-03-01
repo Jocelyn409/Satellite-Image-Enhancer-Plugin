77f7c46c9caa132058ee0da8cca36eb797d10f55
Jocelyn Lahaise <wlahaise@albany.edu>
2024-02-29 22:50:26 -0500

    trying to make radiobutton functionality

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 window.onload = function() {
     document.getElementById("downloadButton").addEventListener("click", print);
     document.getElementById("previewButton").addEventListener("click", print);
+    document.getElementById("previewButton").addEventListener("click", setRadioButton);
 }
 
 function print() {
     console.log("hello wordlddd");
+}
+
+function setRadioButton() {
+    // Find the radio button with the specified ID
+    var radioButton = document.getElementById("two");
+
+    // Check the radio button
+    radioButton.checked = true;
+    console.log(radioButton.checked);
 }
